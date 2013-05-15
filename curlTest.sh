0bc2045612694ec19f6a0776e6084af24d25cef9
hellboy2k8 <shailrishabh@gmail.com>
2013-05-15 11:01:03 +0530

    Tests files moved from / to Tests

--- curlTest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#curl --dump-header  -v -H "Accept: application/json" -H "Content-Type: application/json" -X POST --data "{\"uid\":12,\"token\":\"asdert\"}" http://localhost:5000/v1/
-export http_proxy=''
-
-curl --dump-header  -v -H "Accept: application/json" -H "Content-Type: application/json" -X POST --data "{\"cluster\": {\"node-recipes\": {\"tasktracker\": 3, \"jobtracker\": 1}, \"name\": \"test\", \"image-id\": \"ubuntu-12.04-amd64.img\"}}"  http://localhost:5000/v1/
-
