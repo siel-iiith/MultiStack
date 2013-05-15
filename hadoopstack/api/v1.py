8d8414084245099a5129e72265cb6bb05535b72f
Shashank Sahni <shredder12@gmail.com>
2013-05-15 20:17:04 +0530

    minor fixes in v1.py

--- hadoopstack/api/v1.py
+++ hadoopstack/api/v1.py
@@ -7,15 +7,18 @@ app_v1 = Blueprint('v1', __name__, url_prefix='/v1')
 
 @app_v1.route('/')
 def version():
-    return "v1 API. Jobs and clusters API are accessible at /jobs and /clusters respectively"
+    return "v1 API. Jobs and clusters API are accessible at /jobs and \
+    /clusters respectively"
 
 @app_v1.route('/clusters/', methods = ['GET','POST'])
 def clusters():
     if request.method == 'POST':
         data = request.json
-        number_of_vms = int(data['cluster']['node-recipes']['tasktracker']) + int(data['cluster']['node-recipes']['jobtracker'])        
+        num_tt = int(data['cluster']['node-recipes']['tasktracker'])
+        num_jt = int(data['cluster']['node-recipes']['jobtracker'])        
+        num_vms = num_jt + num_tt
         conn=make_connection()
-        spawn_instances(conn,number_of_vms)
+        spawn_instances(conn,num_vms)
             
         return jsonify(**request.json)    
         
