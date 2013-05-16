dbf11e4d49f680b4a2fc1ebead17210dca74009a
Shashank Sahni <shredder12@gmail.com>
2013-05-16 15:52:53 +0530

    create a separate config file in servies/

--- hadoopstack/services/cluster.py
+++ hadoopstack/services/cluster.py
@@ -1,18 +1,25 @@
 from boto.ec2.connection import EC2Connection
 from boto.ec2.regioninfo import EC2RegionInfo
 
-default_image_id = "ami-0000001a"
-default_min_vm = 1
-master_instance_flavor = "m1.medium"
+import hadoopstack.services.config
 
 def make_connection():
-        url = 'http://10.2.4.129:8773/services/cloud'
-        apikey = 'trial123'
-        passx = 'trial123'
-        pathx='/services/Cloud'
-        hs_region = EC2RegionInfo(name="anythin", endpoint='10.2.4.129:8773')
-        conn=EC2Connection(aws_access_key_id=apikey, aws_secret_access_key=passx, is_secure=False, path=pathx, region=hs_region)
+        url = config.ec2_url
+        url_endpoint = url.split('/')[2]
+        url_port = url.split(':')[2].split('/')[0]
+        url_path = url.split(url_port)[1]    
+
+        hs_region = EC2RegionInfo(name = "siel", endpoint = url_endpoint)
+        
+        conn=EC2Connection(aws_access_key_id=config.access_key, 
+                        aws_secret_access_key=config.secret_key,
+                        is_secure=False, 
+                        path= url_path, 
+                        region=hs_region)
         return conn
 
-def spawn_instances(conn, number=default_min_vm,image_id=default_image_id, flavor=master_instance_flavor):
-    conn.run_instances(default_image_id,int(default_min_vm),int(number),None,None,flavor)
+def spawn_instances(conn, number, 
+                    image_id = config.default_image_id, 
+                    flavor = config.master_instance_flavor):
+
+    conn.run_instances(image_id, int(number), int(number), None, None, flavor)
