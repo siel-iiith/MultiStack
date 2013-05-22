a6167ca7102854a95f75cca26301b2ac07e89414
hellboy2k8 <shailrishabh@gmail.com>
2013-05-23 04:40:24 +0530

    added variable keyName to config.py and changed test.py a bit

--- hadoopstack/tests/test.py
+++ hadoopstack/tests/test.py
@@ -3,15 +3,26 @@ del os.environ['http_proxy']
 
 import requests
 import json
-url = "http://localhost:5000/v1/clusters/"
-url1 = "http://localhost:5000/v1/"
+import sys
+
+
+urlchoice1 = "http://localhost:5000/v1/clusters/"
+urlchoice2= "http://localhost:5000/v1/"
+
+if(sys.argv[1]=="/"):
+	url=urlchoice2
+else:
+	url=urlchoice1
+
+
+urlx = "http://localhost:8000/measure"
 #data = {'sender': 'Alice', 'receiver': 'Bob', 'message': 'We did it!'}
-data={'cluster':{'name':'test','node-recipes':{"jobtracker":1,"tasktracker":3},'image-id': 'ubuntu-12.04-amd64.img'}}
+data={'cluster':{'name':'test','node-recipes':{"jobtracker":1,"tasktracker":0},'image-id': 'ubuntu-12.04-amd64.img'}}
 data_string = json.dumps(data)
 print data_string
 
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-r = requests.post(url1, data=json.dumps(data), headers=headers)
+r = requests.post(url, data=json.dumps(data), headers=headers)
 print r.text
 
 
