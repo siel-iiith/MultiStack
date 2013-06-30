b379b6e6c0788e5e8f1b25573b65b879fd94d2b6
Shashank Sahni <shredder12@gmail.com>
2013-06-30 12:52:00 +0530

    Nothing to be returned for GET api_version/

--- hadoopstack/api/v1.py
+++ hadoopstack/api/v1.py
@@ -1,5 +1,4 @@
-from flask import Blueprint, request, jsonify,current_app
-from flask.ext.pymongo import PyMongo
+from flask import Blueprint, request, jsonify
 
 from hadoopstack.services import job
 from hadoopstack.services import cluster
@@ -13,27 +12,13 @@ from bson import objectid
 
 app_v1 = Blueprint('v1', __name__, url_prefix='/v1')
 
-@app_v1.route('/',methods = ['GET','POST'])
+@app_v1.route('/')
 def version():
     '''
         GET request of the cluster API
     '''
-    if request.method == 'POST':
-        cursor = hadoopstack.main.mongo.db.cluster.find()
-        allEle = []
-        for obj in hadoopstack.main.mongo.db.cluster.find():
-            allEle.append(obj)
-        objToReturn = {}
-        objToReturn['cluster'] = allEle;
-        allTuples = [(objToReturn[i]) for i in objToReturn]
-        allIds = [(str(ids['_id'])) for i in allTuples for ids in i]
-        for i in xrange(0,len(allIds)):
-            allTuples[0][i]['_id'] = allIds[i]
-        return jsonify(**objToReturn)
 
-
-    return "v1 API. Jobs and clusters API are accessible at /jobs and \
-    /clusters respectively"
+    return
 
 @app_v1.route('/jobs', methods=['GET', 'POST'])
 def jobs_api():
