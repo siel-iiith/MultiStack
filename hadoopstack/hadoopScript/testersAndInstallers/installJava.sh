b5ce08a602a8964b2e1e997bbb791bcba24f8330
Shashank Sahni <shredder12@gmail.com>
2013-09-18 19:38:41 +0530

    CleanUp: Removed old hadoop_install scripts
    Signed-off-by: Shashank Sahni <shredder12@gmail.com>

--- hadoopstack/hadoopScript/testersAndInstallers/installJava.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update && sudo apt-get install oracle-jdk7-installer
-update-alternatives –display java
--- hadoopstack/hadoopScript/toPush/hadoop-env.sh
+++
@@ -1,5 +0,0 @@
-export JAVA_HOME=/usr/lib/jvm/java-7-oracle
-export JAVA_HOME=/usr/lib/jvm/java-7-oracle
-export JAVA_HOME=/usr/lib/jvm/java-7-oracle
-export JAVA_HOME=/usr/lib/jvm/java-7-oracle
-export JAVA_HOME=/usr/lib/jvm/java-7-oracle
