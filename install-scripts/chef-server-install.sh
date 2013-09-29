4abd1272a5825912d899e526f35814967055fdf7
Shashank Sahni <shredder12@gmail.com>
2013-09-29 15:47:00 +0530

    Updated chef-server configuration script
    - Fixed knife configuration bugs
    - Uploading both hadoopstack and hadoop cookbooks
    - Removed hardcoded 'ubuntu' user
    
    Signed-off-by: Shashank Sahni <shredder12@gmail.com>

--- install-scripts/chef-server-install.sh
+++ install-scripts/chef-server-install.sh
@@ -1,18 +1,57 @@
-chef_server_ip=`ifconfig eth0 | grep -i 'inet '| cut -d ':' -f 2 | cut -d ' ' -f 1`
+generate_password() {
+    cd /tmp
+    rand1=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1`
+    rand2=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1`
+    echo $rand1$rand2 > $1
+}
 
-wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chef_11.6.0-1.ubuntu.12.04_amd64.deb -O /tmp/chef-client.deb
-wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chef-server_11.0.8-1.ubuntu.12.04_amd64.deb -O /tmp/chef-server.deb
+download_install_chef() {
+	wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chef_11.6.0-1.ubuntu.12.04_amd64.deb -O /tmp/chef-client.deb
+	wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chef-server_11.0.8-1.ubuntu.12.04_amd64.deb -O /tmp/chef-server.deb
+	sudo dpkg -i /tmp/chef-server.deb /tmp/chef-client.deb
+}
 
-sudo dpkg -i /tmp/chef-server.deb /tmp/chef-client.deb
+configure_chef_server() {
+	chef_server_ip=`ifconfig eth0 | grep -i 'inet '| cut -d ':' -f 2 | cut -d ' ' -f 1`
+	chef-server-ctl reconfigure
+}
 
-chef-server-ctl reconfigure
+configure_knife() {
 
-knife configure -u ubuntu --validation-client-name chef-validator --validation-key /etc/chef-server/chef-validator.pem -s https://$chef_server_ip --admin-client-name admin --admin-client-key /etc/chef-server/admin.pem -c /home/ubuntu/.chef/knife.rb -y -r ''
-knife user create -a -s https://localhost -c /home/ubuntu/.chef/knife.rb -u admin -p $RANDOM --disable-editing -k /etc/chef-server/admin.pem ubuntu > /home/ubuntu/.chef/ubuntu.pem
+	mkdir $USER/.chef
+	touch $USER/.chef/knife.rb
 
-sudo apt-get -y install git
-mkdir /tmp/cookbooks
+	knife configure -u $USER \
+	--validation-client-name chef-validator \
+	--validation-key /etc/chef-server/chef-validator.pem \
+	-s https://$chef_server_ip \
+	--admin-client-name admin \
+	--admin-client-key /etc/chef-server/admin.pem \
+	-c $HOME/.chef/knife.rb \
+	-y -r ''
 
-git clone https://github.com/siel-iiith/hadoop-cookbook.git /tmp/cookbooks/hadoop
-git clone https://github.com/siel-iiith/hadoopstack-hadoop-cookbook.git /tmp/cookbooks/hadoopstack
-knife cookbook upload -o /tmp/cookbooks hadoop
+	knife user create \
+	-a \
+	-s https://localhost \
+	-c $HOME/.chef/knife.rb \
+	-u admin \
+	-p generate_password \
+	--disable-editing \
+	-k /etc/chef-server/admin.pem \
+	$USER > $HOME/.chef/$USER.pem
+
+}
+
+upload_cookbooks() {
+	sudo apt-get -y install git
+	mkdir /tmp/cookbooks
+
+	git clone https://github.com/siel-iiith/hadoop-cookbook.git /tmp/cookbooks/hadoop
+	git clone https://github.com/siel-iiith/hadoopstack-hadoop-cookbook.git /tmp/cookbooks/hadoopstack
+	knife cookbook upload -o /tmp/cookbooks -a
+}
+
+download_install_chef
+configure_chef_server
+configure_knife
+upload_cookbooks
