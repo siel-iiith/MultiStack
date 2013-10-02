d3e4e53bb9cd114f7a1301019e4061de8776e071
Shashank Sahni <shredder12@gmail.com>
2013-10-02 17:03:49 +0530

    Rewrote the scheduler
    The scheduler can now handle multicloud(>2) environments and follows
    the new configuration format.
    
    The scheduler works by apply a bunch of filters and take decision.
    Currently, only quota and priority based filters are implemented.
    
    TODO: Do someting similar to openstack's filter scheduler.
    
    Signed-off-by: Shashank Sahni <shredder12@gmail.com>

--- hadoopstack/scheduler/scheduler.py
+++ hadoopstack/scheduler/scheduler.py
@@ -1,81 +1,129 @@
+from multiprocessing import Process
+
+from hadoopstack import config
+from hadoopstack.dbOperations import db
 import hadoopstack
 import hadoopstack.services.cluster as cluster
 
-from multiprocessing import Process
-
-from hadoopstack.services.make_config_parser import configParserHelper
-
-Config = configParserHelper("hadoopstack/flavorConfig.py")
-
 def schedule(data, operation):
-    if(operation == "create"):
-        totalRAM,totalVCPU,totalInstance = calculateUsage(data,operation)
-        cloudDic,decider = priorityCalculator(totalRAM,totalVCPU,totalInstance)
-        if(decider == 1):
-            hadoopstack.main.mongo.db.cloudDetails.save(cloudDic)
+    """Schedules based on certain filters"""
 
-            if(cloudDic['name'] == "openStack"):
-                print "Going to openStack"
-                data['job']['provider'] = "openStack"
-                Process(target = cluster.create, args = (data,"privateCloudConfig")).start()
-                return True
-            else:
-                print "Going to amazonAWS"
-                data['job']['provider'] = "AWS"
-                Process(target = cluster.create, args = (data,"publicCloudConfig")).start()
-                return True
-        else:
+    if(operation == 'create'):
+        conf = config.read_conf()
+
+        clouds = filter_quota(data, conf)
+        if clouds == []:
             return False
 
-    elif(operation == "delete"):
-        totalRAM,totalVCPU,totalInstance = calculateUsage(data,operation)
-        cloudDic = hadoopstack.main.mongo.db.cloudDetails.find_one({'name':data['job']['provider']})
-        cloudDic['ram'] += totalRAM
-        cloudDic['vcpu'] += totalVCPU
-        cloudDic['instance'] += totalInstance
-        hadoopstack.main.mongo.db.cloudDetails.save(cloudDic)
+        cloud = filter_priority(clouds)
+        if cloud is None:
+            return False
 
-        job_id = data['job']['id']
-        Process(target = cluster.delete, args = (job_id,)).start()
+        data['job']['cloud'] = cloud['name']
+        db.flush_data_to_mongo('job', data)
+
+        update_quota(data, cloud, operation)
+        Process(target = cluster.create, args = (data, cloud)).start()
         return True
 
-def priorityCalculator(totalRAM,totalVCPU,totalInstance):
-    for i in range(1,-1,-1):
-        decider = 0
-        cloudDic = hadoopstack.main.mongo.db.cloudDetails.find_one({"priority":i})
-        cloudDic,decider = quotaCalculator(cloudDic,totalRAM,totalVCPU,totalInstance)
-        if(decider == 1):
-            return cloudDic,1
+    elif(operation == 'delete'):
+
+        job_id = data['job']['id']
+
+        conf = config.read_conf()
+        for cloud in conf['clouds']:
+            if cloud['name'] == data['job']['cloud']:
+                break
+
+        Process(target = cluster.delete, args = (job_id, cloud)).start()
+        update_quota(data, cloud, operation)
+        return True
+
+def update_quota(data, cloud, operation):
+    """Update the avaiable quota of a cloud"""
+
+    if operation == 'create':
+        ram, vcpus, instances = calculate_usage(cloud, data)
+        cloud['quota']['available']['ram'] += ram
+        cloud['quota']['available']['vcpus'] += vcpus
+        cloud['quota']['available']['instances'] += instances
+
+    if operation == 'delete':
+        ram, vcpus, instances = calculate_usage(cloud, data)
+        cloud['quota']['available']['ram'] -= ram
+        cloud['quota']['available']['vcpus'] -= vcpus
+        cloud['quota']['available']['instances'] -= instances
+
+    conf = config.read_conf()
     
-    return {},0
+    for i in range(0, len(conf['clouds'])):
+        if conf['clouds'][i]['id'] == cloud['id']:
+            conf['clouds'][i] = cloud
 
-    pass
+    db.flush_data_to_mongo('conf', conf)
 
-def quotaCalculator(cloudDic,totalRAM,totalVCPU,totalInstance):
-    if(totalRAM<cloudDic['ram'] and totalVCPU<cloudDic['vcpu'] and totalInstance<cloudDic['instance']):
-        cloudDic['ram'] -=totalRAM
-        cloudDic['vcpu'] -=totalVCPU
-        cloudDic['instance'] -=totalInstance
-        return cloudDic,1
-        print cloudDic
-        print "can be done"
+def filter_priority(clouds):
+    """Returns the cloud with the hightes priority"""
 
-def calculateUsage(data,operation):
-    totalVCPUs = 0
-    totalRAM = 0
-    totalInstance = 0
-    master = data['job']['master']['flavor']
-    totalRAM += int(Config.get(master,"ram"))
-    totalVCPUs += int(Config.get(master,"vcpu"))
-    totalInstance += 1
+    priority = int(clouds[0]['priority'])
+    qualified_cloud = clouds[0]
+
+    for cloud in clouds:
+        if int(cloud['priority']) < priority:
+            priority = int(cloud['priority'])
+            qualified_cloud = cloud
+
+    return qualified_cloud
+
+def filter_quota(data, conf):
+    """
+    Return the list of cloud objects which satisfy the resource required.
+    """
+
+    qualified_clouds = list()
+
+    for cloud in conf['clouds']:
+        if cloud_satisfy_quota(cloud, data):
+            qualified_clouds.append(cloud)
+
+    return qualified_clouds
+
+def cloud_satisfy_quota(cloud, data):
+    """
+    Based on the resources required by the job, check if the current
+    cloud object satisfies the requirement
+    """
+
+    ram, vcpus, instances = calculate_usage(cloud, data)
+    available_quota = cloud['quota']['available']
+
+    if ((ram < available_quota['ram']) and
+        (vcpus < available_quota['vcpus']) and
+        (instances < available_quota['instances'])):
+
+        return True
+
+    else:
+        return False
+
+def calculate_usage(cloud, data):
+    """
+    This function calculates resource requirement of the job.
+    """
+
+    job_ram = 0
+    job_vcpus = 0
+    job_instances = 0
+
+    master_flavor = data['job']['master']['flavor'].replace('.', '_')
+    job_ram += cloud['flavors'][master_flavor]['ram']
+    job_vcpus += cloud['flavors'][master_flavor]['vcpus']
+    job_instances += 1
 
     for slave in data['job']['slaves']:
-        totalRAM += int(Config.get(slave['flavor'],"ram"))
-        totalVCPUs += int(Config.get(slave['flavor'],"vcpu"))
-        totalInstance += 1
+        slave_flavor = slave['flavor'].replace('.', '_')
+        job_ram += cloud['flavors'][slave_flavor]['ram']
+        job_vcpus += cloud['flavors'][slave_flavor]['vcpus']
+        job_instances += slave['instances']
 
-    return totalRAM,totalVCPUs,totalInstance
-
-
-
-
+    return job_ram, job_vcpus, job_instances
