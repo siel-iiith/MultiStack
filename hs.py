5962f34674dc034096291b61eb48ecf350cbd9c8
Shashank Sahni <shredder12@gmail.com>
2013-10-06 19:29:31 +0530

    enabled reloader by default in flask.app
    Signed-off-by: Shashank Sahni <shredder12@gmail.com>

--- hs.py
+++ hs.py
@@ -4,4 +4,4 @@ from hadoopstack import config
 with app.app_context():
 	config.set_conf()
 
-app.run(host='0.0.0.0', port = 5000, debug = True, use_reloader = False)
+app.run(host='0.0.0.0', port = 5000, debug = True, use_reloader = True)
